2925f8391695319bab6e1ca84b8f31a18c449593
Matheus Llobregat Carvalho <mllcarvalho@MacBook-Air-de-Matheus.local>
2024-03-23 11:41:38 -0300

    spring intro

---
+++ algaworks-spring-intro/src/main/java/com/treinamentosping/awpag/AwpagApplication.java
@@ -0,0 +1,13 @@
+package com.treinamentosping.awpag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwpagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwpagApplication.class, args);
+	}
+
+}
---
+++ algaworks-spring-intro/src/main/java/com/treinamentosping/awpag/domain/model/Cliente.java
@@ -0,0 +1,15 @@
+package com.treinamentosping.awpag.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cliente {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+}
