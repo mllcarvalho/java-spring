d1f493e43c9bb17bf8f41c2af1734ad8c5cfdbcb
Matheus Llobregat Carvalho <mllcarvalho@MacBook-Air-de-Matheus.local>
2024-03-30 09:04:19 -0300

    Criando camada de persistencia

--- algaworks-spring-ignicao/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ algaworks-spring-ignicao/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,6 +1,7 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,24 +18,7 @@ public class ProprietarioController {
 
     @GetMapping("/proprietarios")
     public ResponseEntity<List<Proprietario>> listar() {
-
-        var proprietarioList = new ArrayList<Proprietario>();
-
-        var proprietario1 = new Proprietario();
-        proprietario1.setId(1L);
-        proprietario1.setNome("Matheus");
-        proprietario1.setEmail("mlc@gmail.com");
-        proprietario1.setTelefone("11988887777");
-
-        var proprietario2 = new Proprietario();
-        proprietario2.setId(2L);
-        proprietario2.setNome("Adriana");
-        proprietario2.setEmail("agmf@gmail.com");
-        proprietario2.setTelefone("11944445555");
-
-        proprietarioList.add(proprietario1);
-        proprietarioList.add(proprietario2);
-
+        List<Proprietario> proprietarioList = new ArrayList<>();
         return ResponseEntity.ok(proprietarioList);
     }
 }
--- algaworks-spring-ignicao/src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
+++ algaworks-spring-ignicao/src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -1,12 +1,22 @@
 package com.algaworks.algatransito.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Proprietario {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
     public String nome;
     public String email;
