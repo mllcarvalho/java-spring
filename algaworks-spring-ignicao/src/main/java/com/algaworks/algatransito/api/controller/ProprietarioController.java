a644eb0ee6abe174f5bc3e55e0e157f0510993c3
Matheus Llobregat Carvalho <mllcarvalho@MacBook-Air-de-Matheus.local>
2024-03-30 17:56:05 -0300

    implementação métodos listar e buscar

--- algaworks-spring-ignicao/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ algaworks-spring-ignicao/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,24 +1,39 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 import jakarta.persistence.EntityManager;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/proprietarios")
 @AllArgsConstructor
 public class ProprietarioController {
 
-    @GetMapping("/proprietarios")
-    public ResponseEntity<List<Proprietario>> listar() {
-        List<Proprietario> proprietarioList = new ArrayList<>();
-        return ResponseEntity.ok(proprietarioList);
+    private final ProprietarioRepository proprietarioRepository;
+
+    @GetMapping
+    public List<Proprietario> listar() {
+
+        return proprietarioRepository.findAll();
+    }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> Buscar(@PathVariable Long proprietarioId) {
+
+        return proprietarioRepository.findById(proprietarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
