9e7a33ba47fed739a421ad0848bc4e1e64f10688
Matheus Llobregat Carvalho <mllcarvalho.it@gmail.com>
2024-04-02 06:27:52 -0300

    :recycle: refactor: ExceptionHandler global

--- algaworks-spring-ignicao/src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
+++ algaworks-spring-ignicao/src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
@@ -67,9 +67,4 @@ public class VeiculoController {
         registroVeiculoService.excluir(veiculoId);
         return ResponseEntity.noContent().build();
     }
-
-    @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<String> capturar(NegocioException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
 }
