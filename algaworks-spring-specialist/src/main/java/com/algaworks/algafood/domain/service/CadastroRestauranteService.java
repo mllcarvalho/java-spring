2383515598bac51435e24e29f53482ae9e9115a5
Matheus Llobregat Carvalho <mllcarvalho.it@gmail.com>
2024-04-25 23:29:51 -0300

    feat: ajustes dependencias

--- algaworks-spring-specialist/src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ algaworks-spring-specialist/src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -1,13 +1,11 @@
 package com.algaworks.algafood.domain.service;
 
-import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
