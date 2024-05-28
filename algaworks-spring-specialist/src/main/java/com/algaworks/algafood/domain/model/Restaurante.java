9e08a3205937c332fd7a5ccfb93b83cfa8d8db5e
Matheus Llobregat Carvalho <mllcarvalho.it@gmail.com>
2024-05-28 15:30:13 -0300

    feat: Lazy and Eager loading | fetch join

--- algaworks-spring-specialist/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algaworks-spring-specialist/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -30,7 +31,9 @@ public class Restaurante {
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
-    @ManyToOne
+    //@JsonIgnoreProperties("hibernateLazyInitializer")
+    //@JsonIgnore
+    @ManyToOne//(fetch = FetchType.LAZY)
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
@@ -48,7 +51,7 @@ public class Restaurante {
     @Column(nullable = false, columnDefinition = "datetime")
     private LocalDateTime dataAtualizacao;
 
-    @JsonIgnore
+    //@JsonIgnore
     @ManyToMany
     @JoinTable(name = "restaurante_forma_pagamento",
             joinColumns = @JoinColumn(name = "restaurante_id"),
